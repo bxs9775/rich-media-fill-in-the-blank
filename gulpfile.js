52caaffea0915d6cec19b38c5bee6fba55a8f8c2
bxs9775 <bxs9775@rit.edu>
2018-04-04 09:36:25 -0400

    Created a gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+//Gulp file for building scripts
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const nodemon = require('gulp-nodemon');
+const eslint = require('gulp-eslint');
+const concat = require('gulp-concat');
+
+//Gulp tasks
+gulp.task('buildApp',() => {
+  gulp.src(['./client/app/*.js','./client/helper/*.js'])
+  .pipe(babel({
+    presets: ['env','react']
+  }))
+  .pipe(concat('bundle.js'))
+  .pipe(gulp.dest('./hosted'));
+});
+
+gulp.task('buildLogin',() => {
+  gulp.src(['./client/login/*.js','./client/helper/*.js'])
+  .pipe(babel({
+    presets: ['env','react']
+  }))
+  .pipe(concat('loginBundle.js'))
+  .pipe(gulp.dest('./hosted'));
+});
+
+gulp.task('js',() => {
+  gulp.start('buildApp');
+  gulp.start('buildLogin');
+});
+
+gulp.task('lint',() => {
+  return gulp.src(['./server/*.js'])
+  .pipe(eslint())
+  .pipe(eslint.format())
+  .pipe(eslint.failAfterError());
+});
+
+gulp.task('build',() => {
+  gulp.start('js');
+  gulp.start('lint');
+});
+
+gulp.task('watch', () => {
+  gulp.watch('./client/app/*.js',['buildApp']);
+  gulp.watch('./client/login/*.js',['buildLogin']);
+  gulp.watch('./client/helper/*.js',['js']);
+  
+  nodemon({
+    script: './server/app.js',
+    ext: 'js',
+    tasks: ['lint']
+  });
+});
+
+gulp.task('watch')
